e29df93badaabcf25842ef7ed5756e5f9523e893
cheetah <contact@cheetahbyte.dev>
2023-07-16 17:17:15 +0200

    feat(events): add event types (#14)
    - added `rpc` event type
    - added `basic` event type
    
    Closes #13

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("Readme.md") as f:
 
 setup(
     name="miauw-base-service",
-    version="1.7.2",
+    version="1.8.0",
     author="cheetahbyte",
     author_email="contact@cheetahbyte.dev",
     description="base service & worker class for miauw-social",
