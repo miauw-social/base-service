33a1afff0928e1ad6a6561127bdc38891789f880
Leo Breuer <contact@cheetahbyte.dev>
2023-07-18 08:43:12 +0200

    feat(database): add functions for database pooling
    - add `psycopg` driver
    - add `BaseService().pool` to interact with the database pool

--- base_service/service.py
+++ base_service/service.py
@@ -1,26 +1,35 @@
 from base_service.email_worker import EmailTemplates, EMailWorker
 from base_service.worker import RabbitMQWorker
-from collections import defaultdict
+from base_service.utils import get_file_paths, build_prepared_statement
+from psycopg_pool import AsyncConnectionPool
+from psycopg import AsyncConnection
 import asyncio
 import typing
 import logging
 import os
 
+
 class BaseService:
     """creates the base service class"""
 
     def __init__(
-        self, name: str, url: str | None= os.getenv("RABBITMQ_URL"), logfile: str = None, worker_log: bool = True
+            self, name: str, rabbitmq_url: str | None = os.getenv("RABBITMQ_URL"),
+            postgres_url: str | None = os.getenv("POSTGRES_URL"), logfile: str = None, worker_log: bool = True
     ):
-        if not url:
+        if not rabbitmq_url:
             raise Exception("No URL provided and env var 'RABBITMQ_URL' is empty.")
-        self.worker = RabbitMQWorker(url)
-        self.emailer = EMailWorker(url)
+        if postgres_url:
+            self.postgres_url = postgres_url
+        else:
+            self.postgres_url = None
+        self.worker = RabbitMQWorker(rabbitmq_url)
+        self.emailer = EMailWorker(rabbitmq_url)
         self.events: list[str] = []
         self.m: dict[
-            str, typing.Callable[[typing.Any], typing.Awaitable[typing.Any]]
-        ] = defaultdict(rpc={}, basic={})
+            str, dict[typing.Callable[[typing.Any], typing.Awaitable[typing.Any]]]
+        ] = dict(rpc={}, basic={})
         self.name = name
+        self._pool: AsyncConnectionPool | None = None
         # logging
         self.logger = logging.getLogger(name)
         self.log_formatter = logging.Formatter("[%(asctime)s] - %(name)s - %(message)s")
@@ -38,11 +47,47 @@ class BaseService:
         """starts the service"""
         self.logger.info("starting")
         loop = asyncio.get_event_loop()
+
         for ev in self.events:
             self.logger.info(f"listening for event '{ev}'")
         loop.run_until_complete(
-            asyncio.gather(*[self.worker.listen(event, handler) for a in self.m.values() for event, handler in a.items()])
+            asyncio.gather(
+                *[self.worker.listen(event, handler) for a in self.m.values() for event, handler in a.items()])
         )
+
+    async def init_db(self,
+                      prepare_statements: bool = True,
+                      path: str = "./sql/",
+                      include_sub_dirs: bool = True,
+                      exclude_files: list[str] = [],
+                      min_size: int = 5,
+                      max_size: int = 15):
+        """initialises the connection with the database and prepares the statements."""
+        self._pool = AsyncConnectionPool(self.postgres_url, min_size=5, max_size=15)
+        # statements: list[str] = []
+        # if prepare_statements:
+        #     files = get_file_paths(path, walk_sub_dirs=include_sub_dirs)
+        #     for file in files:
+        #         if file in exclude_files:
+        #             continue
+        #         else:
+        #             with open(file) as f:
+        #                 statements.append(build_prepared_statement(file.lstrip(f"{path}"), f.read()))
+        # conn: AsyncConnection
+        # for statement in statements:
+        #     print(statement)
+        # async with self.pool.connection() as conn:
+        #     for statement in statements:
+        #         await conn.execute(statement)
+        #         await conn.commit()
+        #         print("prepared statement", statement)
+
+    @property
+    def pool(self):
+        """returns the database connection pool of the service"""
+        if not self._pool:
+            raise AttributeError("You need to call BaseService.init_db() first.")
+        return self._pool
 
     def event(self, event: str, event_type: str = "rpc"):
         """adds a new event handler for event"""
@@ -51,4 +96,5 @@ class BaseService:
             self.logger.debug(f"add handler {handler} for event '{event}'")
             self.events.append(event)
             self.m[event_type][event] = handler
+
         return wrapper
